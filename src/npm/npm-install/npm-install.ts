03d39083d0eb4585df867e1161b36fafef097290
Charlie Greenman <charlie@razroo.com>
2023-08-17 16:33:21 -0700

    [ZETA-6460]: Eureka! Update package json based on command (#7)
    * ZETA-6460: Update package json name.
    
    * ZETA-6460: Add codemorph core.
    
    * ZETA-6460: Benchmark progress towards completing json logic.
    
    * ZETA-6460: Wire up inital npm install update logic.

--- src/npm/npm-install/npm-install.ts
+++ src/npm/npm-install/npm-install.ts
@@ -1,12 +1,28 @@
 import { extractPackageName } from "../../utils/command-search/command-search";
 import { getNameAndVersion } from "../../utils/version/version";
+import {morphCode, EditInput} from "@codemorph/core";
 
-export async function npmInstallCodemorph(commandText: string, stringToUpdate: string): Promise<string> {
+export async function npmInstallCodemorph(commandText: string, packageJsonString: string): Promise<string> {
   const packageName = extractPackageName(commandText);
   const {name, version} = await getNameAndVersion(packageName);
+
+  const editInput: EditInput = {
+    fileType: 'json',
+    fileName: 'package.json',
+    fileToBeAddedTo: packageJsonString,
+    edits: [
+      {
+        nodeType: 'editJson' as any,
+        valueToModify: '/dependencies',
+        codeBlock: {[name]: version}
+      }
+    ]
+  }
+  
+  const morphedCode = morphCode(editInput);
   console.log('name');
   console.log(name);
   console.log('version');
   console.log(version);
-  return commandText;
+  return morphedCode;
 }
