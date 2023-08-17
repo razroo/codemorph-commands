4b592c994b65d7fcda2c13573066da161309205a
Charlie Greenman <charlie@razroo.com>
2023-08-17 14:34:44 -0700

    ZETA-6456: Fix unit tests.

--- src/npm/npm-install/npm-install.ts
+++ src/npm/npm-install/npm-install.ts
@@ -1,7 +1,5 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 export function npmInstallCodemorph(commandText: string): string {
-  const commandText = 'npm install test;'; 
-  const npmPackage = 
   return commandText;
 }
