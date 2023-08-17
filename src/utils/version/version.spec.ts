13b79df52986003f657129fb8bd889bf9fec9f3d
Charlie Greenman <charlie@razroo.com>
2023-08-17 14:29:48 -0700

    ZETA-6456: Add more unit tests for getting the name and version.

--- src/utils/version/version.spec.ts
+++ src/utils/version/version.spec.ts
@@ -1,10 +1,17 @@
 import { extractNameAndVersion } from "./version";
 
 describe('Version', () => {
-  it('should extract the package name and version if at sign in beginning', () => {
+  it('should extract the package name and version if YES at sign in beginning', () => {
     const packageNameAndVersion = '@apollo/client@^3.3.19';
     const result = extractNameAndVersion(packageNameAndVersion);
     const expected = { name: "@apollo/client", version: "^3.3.19" };
+    expect(result).toEqual(expected);
+  });
+
+  it('should extract the package name and version if NO at sign in beginning', () => {
+    const packageNameAndVersion = 'apollo-angular@^3.0.1';
+    const result = extractNameAndVersion(packageNameAndVersion);
+    const expected = { name: "apollo-angular", version: "^3.0.1" };
     expect(result).toEqual(expected);
   });
 });
