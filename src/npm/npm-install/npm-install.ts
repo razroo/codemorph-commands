b39beb7d244c392612715594bd8f0575f2d493f9
Charlie Greenman <charlie@razroo.com>
2023-08-17 15:58:23 -0700

    ZETA-6460: Update package json name.

--- src/npm/npm-install/npm-install.ts
+++ src/npm/npm-install/npm-install.ts
@@ -1,7 +1,7 @@
 import { extractPackageName } from "../../utils/command-search/command-search";
 import { getNameAndVersion } from "../../utils/version/version";
 
-export async function npmInstallCodemorph(commandText: string, stringToUpdate: string): Promise<string> {
+export async function npmInstallCodemorph(commandText: string, packageJsonString: string): Promise<string> {
   const packageName = extractPackageName(commandText);
   const {name, version} = await getNameAndVersion(packageName);
   console.log('name');
