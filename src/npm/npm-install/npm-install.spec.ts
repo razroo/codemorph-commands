cdb2ec24bf7b0b321d30c0ee449ab5be42aa36cf
Charlie Greenman <charlie@razroo.com>
2023-08-31 15:57:15 -0700

    ZETA-6608: Add save-dev support for npm install.

--- src/npm/npm-install/npm-install.spec.ts
+++ src/npm/npm-install/npm-install.spec.ts
@@ -2,7 +2,7 @@ import { morphCommand } from "../../command-morph/command-morph";
 
 describe('npmInstall', () => {
   it('should return npm install with needed codemod', async() => {
-    const npmInstallCommandText = 'npm install apollo-angular react --save';
+    const npmInstallCommandText = 'npm install apollo-angular@5.0.0 react --save';
     const mockPackageJson = {
       name: 'hello', 
       dependencies: {
@@ -15,6 +15,29 @@ describe('npmInstall', () => {
   "name": "hello",
   "dependencies": {
     "angular": "16.0.0",
+    "apollo-angular": "5.0.0",
+    "react": "18.2.0"
+  }
+}`;
+    expect(result).toEqual(expected);
+  });  
+
+  it('should return npm install with needed codemod if saving as dev dependency', async() => {
+    const npmInstallCommandText = 'npm install apollo-angular@5.0.0 react@18.2.0 --save-dev';
+    const mockPackageJson = {
+      name: 'hello', 
+      dependencies: {
+        "angular": "16.0.0"
+      }
+    };
+    const stringifiedMockPackageJson = JSON.stringify(mockPackageJson);
+    const result = await morphCommand(npmInstallCommandText, stringifiedMockPackageJson);
+    const expected = `{
+  "name": "hello",
+  "dependencies": {
+    "angular": "16.0.0"
+  },
+  "devDependencies": {
     "apollo-angular": "5.0.0",
     "react": "18.2.0"
   }
